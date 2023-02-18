724a2247f365f424dfaa93219e134b302b9eecd3
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-02-18 16:59:57 +0300

    I don't know what i did here. I'm just trying to figure out how the fuck vercel deployment works

---
+++ build/precache-manifest.53f819dcae91be941ae150a79c48bf34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b07cedf11e7e7bc7a43f362bf51789",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e160f586050f7d6a261d",
+    "url": "/static/css/main.ca8cd1e8.chunk.css"
+  },
+  {
+    "revision": "9483dcf926037ae93ee2",
+    "url": "/static/js/2.43162cb4.chunk.js"
+  },
+  {
+    "revision": "e160f586050f7d6a261d",
+    "url": "/static/js/main.ae6c5b4e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
